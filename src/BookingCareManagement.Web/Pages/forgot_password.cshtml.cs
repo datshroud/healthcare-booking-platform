16d941523065affe10e5c9e5df6e2f1d75bc4654
dai2111 <tdtf314@gmail.com>
2025-10-30 23:51:40 +0700

    thêm form đăng nhập đăng ký

---
+++ src/BookingCareManagement.Web/Pages/forgot_password.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookingCareManagement.Pages
+{
+    public class forgot_passwordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
